438fc6568d4865a3b187ccd517b0c2843e433cd6
wjddustn1 <jang03298625@gmail.com>
2025-09-10 17:34:09 +0900

    Add files via upload

---
+++ chap01/src/sec01/exam04/ScopeTest.java
@@ -0,0 +1,23 @@
+package sec01.exam04;
+
+public class ScopeTest {
+
+	public static void main(String[] args) {
+		int a = 10;
+		///int b = 20;
+		
+		if(a>1) 
+		{
+			int b = a;
+		}
+		
+		for(int i = 0; i< 10; i++) 
+		{
+		    int c = a + i;
+ 			///c = b + i;
+		}
+		
+
+	}
+
+}
---
+++ chap01/src/sec03/exam01/PromotionTest.java
@@ -0,0 +1,24 @@
+package sec03.exam01;
+
+public class PromotionTest {
+
+	public static void main(String[] args) {
+		byte byteVal = 10;
+		int intVal = byteVal;
+		//byte byteVall = intVal;
+		
+		char charVal = 'A';
+		//char charVal = 65;
+		//System.out.println(charVal);
+		int intVall = charVal;
+	    System.out.println((char)intVall);
+	    
+	    byte byteVal1 = 65;
+	    int intVal2 = 65;
+	    char charVal2 = (char)byteVall;
+	    char charVAl3 = byteVal2;
+	
+
+	}
+
+}
