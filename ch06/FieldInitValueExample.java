e30744a96de832d2a9ed7275960a82fa5409639d
wjddustn1 <jang03298625@gmail.com>
2025-12-12 22:56:59 +0900

    개인실습

---
+++ ch06/FieldInitValueExample.java
@@ -0,0 +1,10 @@
+package ch06;
+
+public class FieldInitValueExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ch06/KoreanExample.java
@@ -0,0 +1,13 @@
+package ch06;
+
+public class KoreanExample {
+    public static void main(String[] args) {
+        Korean k1 = new Korean("박자바", "011225-1234567");
+        System.out.println("첫 번째 사람 이름: " + k1.name);
+        System.out.println("첫 번째 사람 주민번호: " + k1.ssn);
+
+        Korean k2 = new Korean("김자바", "930525-0654321");
+        System.out.println("두 번째 사람 이름: " + k2.name);
+        System.out.println("두 번째 사람 주민번호: " + k2.ssn);
+    }
+}
